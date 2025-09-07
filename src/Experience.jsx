22585f0f300b19789fceea0a90e76d4cec3b44ad
setareh <setarehsaboory@gmail.com>
2025-09-07 06:36:31 +0200

    added title and icon

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,7 +3,7 @@ import ComputerMdl from './components/ComputerMdl'
 
 export default function Experience() {
     return <>
-        <color args={['#241a1a']} attach="background" />
+        <color args={['#011605']} attach="background" />
         <Environment preset='city' />
         <ComputerMdl />
         <ContactShadows position={[0, -0.3, 0]} scale={6} opacity={0.8} color={"#000000"} blur={1.5} />

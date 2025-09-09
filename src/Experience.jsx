9e38185af1e793fea30595a5303d282e41c10937
setareh <setarehsaboory@gmail.com>
2025-09-09 04:34:32 +0200

    responsiveness

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,13 +1,17 @@
 import { ContactShadows, Environment } from '@react-three/drei'
 import ComputerMdl from './components/ComputerMdl'
 
-export default function Experience() {
-    return <>
-        <color args={['#011605']} attach="background" />
-        <Environment preset='city' />
-        <ComputerMdl />
-        <ContactShadows position={[0, -0.3, 0]} scale={6} opacity={0.8} color={"#000000"} blur={1.5} />
 
-    </>
+export default function Experience() {
+
+    return (
+        <>
+            <color args={['#011605']} attach="background" />
+            <Environment preset='apartment' />
+            <ComputerMdl />
+            <ContactShadows position={[0, -0.3, 0]} scale={6} opacity={0.8} color={"#000000"} blur={1.5} />
+
+        </>
+    )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,29 @@ import './style.scss';
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import { Perf } from 'r3f-perf';
+import RotateOverlay from './components/RotateOverlay/RotateOverlay';
+import useBreakpoint from './components/Hooks/useBreakpoint.js';
+import useOrientation from './components/Hooks/useOrientation.js';
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+function App() {
+  const breakpoint = useBreakpoint();
+  const orientation = useOrientation();
+  const rotateIsNeeded = breakpoint === 'mobile' && orientation === 'portrait';
 
-root.render(
-    <Canvas
-    className="CanvasCls"
-
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 2000,
-            position: [ -3, 1.5, 4 ]
-        } }
-    >
+  return (
+    <>
+       <RotateOverlay show={rotateIsNeeded} />
+      <Canvas
+        className="CanvasCls"
+        camera={{ fov: 45, near: 0.1, far: 2000, position: [-3, 1.5, 4] }}
+      >
+        <Perf />
         <Experience />
-    </Canvas>
-)
+      </Canvas>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<App />);
